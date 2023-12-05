3fa7d239ed79fba1df2790efd72f03240ce1ead6
Nikita <ppdbavyss@vk.com>
2023-12-05 20:12:42 +0400

    add BubbleSort

--- include/Sorting/Sorts.cpp
+++ include/Sorting/Sorts.cpp
@@ -12,4 +12,50 @@ namespace sorting {
 		size_t copy_count = 0;
 	};
 
+	template <typename T>
+	void custom_swap(T& a, T& b) {
+		T temp = a;
+		a = b;
+		b = temp;
+	};
+
+	ostream& operator<<(ostream& os, stats& stat)
+	{
+		os << "comparison_count = " << stat.comparison_count << " copy_count = " << stat.copy_count;
+		return os;
+	}
+
+	class ReturnClass {
+	private:
+		int value;
+	public:
+		ReturnClass() :value(0) {};
+		ReturnClass(const int value) :value(value) {};
+		int get_value() const {
+			return this->value;
+		}
+		bool operator>(const ReturnClass& rhs) {
+			return this->value > rhs.value;
+		}
+		bool operator<(const ReturnClass& rhs) {
+			return !(*this > rhs);
+		}
+	};
+
+	template<typename T>
+	stats& BubbleSort(vector<T>& vec) {
+		stats statistics;
+		for (int i(0); i < (vec.size() - 1); ++i) {
+			for (int j(0); j < (vec.size() - i - 1); ++j) {
+				statistics.comparison_count += 1;
+				if (vec[j] > vec[j + 1]) {
+					custom_swap(vec[j], vec[j + 1]);
+					//statistics.copy_count += 3;???
+					statistics.copy_count += 2;//???
+				}
+			}
+		}
+		return statistics;
+	}
+
 };
--- main.cpp
+++ main.cpp
@@ -1,11 +1,33 @@
 ﻿#include <Sorts.cpp>
 #include <ctime>
 using namespace std;
-
 using namespace sorting;
 
-int main()
-{
-	cout << "Hello CMake." << endl;
-	return 0;
+void static do_Bubble(){
+	srand(412);
+	vector<int> test_vector1;
+	size_t test_vector_size = 50;
+
+	for (int i = 0; i < test_vector_size; ++i) {
+		test_vector1.push_back(rand() % 100);
+	}
+
+	cout << "Случайные значения в векторе:" << endl;
+	for (int i = 0; i < test_vector1.size(); ++i) {
+		cout << test_vector1[i] << " ";
+	}
+	cout << endl << endl;
+
+	stats sorting_stats1 = BubbleSort(test_vector1);
+	cout << "Отсортированный массив (BubbleSort) :\n";
+	for (int i = 0; i < test_vector1.size(); ++i) {
+		cout << test_vector1[i] << " ";
+	}
+	cout << endl << endl;
+	cout << sorting_stats1 << endl << endl << endl;
+	}
+
+int main() {
+	setlocale(LC_ALL, "ru");
+	do_Bubble();
 }
